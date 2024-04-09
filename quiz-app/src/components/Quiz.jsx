ad8bf03665384757f296ee5457684b02cff4b876
pushyanth <90887929+sajjapushyanth@users.noreply.github.com>
2024-04-09 20:08:57 +0530

    final commit

--- quiz-app/src/components/Quiz.jsx
+++ quiz-app/src/components/Quiz.jsx
@@ -6,35 +6,76 @@ export default function Quiz(){
     const [index,setIndex]=useState(0)
     const [question,setQuestion]=useState(data[index])
     const [selected,setSelected]=useState(null)
+    const [isCorrect,setIsCorrect]=useState(null)
+    const [result,setResult]=useState(0)
+    const [answered,setAnswered]=useState(false)
+
+    console.log("result : ",result)
+
 
     function nextQuestion(){
         setIndex(index+1)
-        setQuestion(data[index+1])
-    }
-    function list(ans){
-        if(question.ans===ans){
-            console.log("true")
-            setSelected(ans)
+        if(index+1===5){
+            setQuestion(null)
         }
         else{
-            console.log("false")
+            
+        setQuestion(data[index+1])
+        }
+        setSelected(null)
+        setIsCorrect(null)
+        setAnswered(false)
+    }
+    function list(ans){
+        if(!answered){
+            
+        if(question.ans===ans){
+            setIsCorrect(true)
+            setResult(prevResult=>prevResult+1)
+            console.log("true")
             
         }
+        else{
+            setIsCorrect(false)
+            
+        }
+        setSelected(ans)
+        setAnswered(true)
+
+        }
+    }
+
+    function getClassNames(option,selected,isCorrect){
+        if(selected===option &&isCorrect!==null){
+            if(!isCorrect){
+                return "wrong"
+            }
+            return "selected"
+        }
+        return ""
+
     }
     return(
         
         <div className="quiz-container">
             <h1 className="title">Quiz-App</h1>
             <hr className="hr"/>
-            <h2 className="quiz-question">{index+1} .{question.question}</h2>
+            {question?(
+                <>
+                <h2 className="quiz-question">{index+1} .{question.question}</h2>
             <ul className="quiz-options">
-                <li className={`quiz-list ${selected===1 && question.ans===1 ? "selected":""}`} onClick={()=>list(1)}>{question.option1}</li>
-                <li className={`quiz-list ${selected===2 && question.ans===2 ? "selected":""}`} onClick={()=>list(2)}>{question.option2}</li>
-                <li className={`quiz-list ${selected===3 && question.ans===3 ? "selected":""}`} onClick={()=>list(3)}>{question.option3}</li>
-                <li className={`quiz-list ${selected===4 && question.ans===4 ? "selected":""}`} onClick={()=>list(4)}>{question.option4}</li>
+                <li className={`quiz-list ${getClassNames(1,selected,isCorrect)}`} onClick={()=>list(1)}>{question.option1}</li>
+                <li className={`quiz-list ${getClassNames(2,selected,isCorrect)}`} onClick={()=>list(2)}>{question.option2}</li>
+                <li className={`quiz-list ${getClassNames(3,selected,isCorrect)}`} onClick={()=>list(3)}>{question.option3}</li>
+                <li className={`quiz-list ${getClassNames(4,selected,isCorrect)}`} onClick={()=>list(4)}>{question.option4}</li>
+                
             </ul>
             <button className="next" onClick={nextQuestion}>Next</button>
             <h3 className="next-page">{index+1} of {data.length} Question </h3>
+                </>
+            ):
+            (<div className="result"> Your Result is : <b>{result}</b> </div>)}
+            
         </div>
     )
 }
